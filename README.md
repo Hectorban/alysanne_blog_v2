4382ac6e3bbe47a90cad7133b721d8d31519781a
Héctor Banderas <hectorban1234@gmail.com>
2024-06-06 21:36:05 -0600

    feat: cool hexagram animation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alysanne
+
+I will mainly making esoteric works and a side of fragance and tech opinions
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,13 +10,9 @@ export default function Navbar() {
         <li id={styles.name}>
           <h1 className={main_font.className}>
             <Link className={styles.link} href="/">
-              <span>Alysanne</span>
-              <Image
-                src="/hexagram.png"
-                width={30}
-                height={30}
-                alt="Hexagram"
-              />
+              <svg className={styles.svg} viewBox="0 0 194.21 221" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <path  d="M96.853 20.5l74.531 127.929L22.321 72.57 96.853 200.5l74.531-127.929L22.321 148.43 96.853 20.5z" fill="none" stroke="#FFFFFF" strokeWidth="7"/>
+              </svg>
             </Link>
           </h1>
         </li>
